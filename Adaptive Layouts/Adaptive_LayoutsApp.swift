f80b0a53a218fa1a43898a9bf14cecad9d0f5c57
Nikita Yankov <evne.yankovnikita@gmail.com>
2023-03-04 00:13:34 +0200

    Initial Commit

---
+++ Adaptive Layouts/Adaptive_LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adaptive_LayoutsApp.swift
+//  Adaptive Layouts
+//
+//  Created by nikita on 04.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Adaptive_LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
